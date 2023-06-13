a21056124e131ea11c9d66aec0175c6e1970328f
Harshitha pulipakala <129891239+Harshitha19323@users.noreply.github.com>
2023-06-13 23:36:51 +0530

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,53 @@
+print("Helo world!")
+##print strings 1.1
+print("Day 1 - python print Function")
+print("The function is declared like this")
+print("print('what to print')")
+##multiple statements
+print("Hello world\nHello world\nhello world")
+##concatenate
+print("hello"+"world")
+print("phoebe"+" "+"Harshitha")
+##debugging 1.2
+##print(Day1 - string manipulation")
+##print("striing concatenation is done with the "+" sign.')
+##print('e.g.print("hello" +"world"')
+##fixing th code
+print(("new lines can be created with a backslash and n"))
+print("Day1 - string manipulation")
+print("striing concatenation is done with the "+" sign.")
+print('e.g.print("hello" +"world")')
+print(("new lines can be created with a backslash and n"))
+##input 1.3
+input("WHAT IS YOUR NAME? ")
+##STRING LENGTH
+print(len("harshitha sateesh"))
+print(len(input("what is your name?")))
+##variables 1.4
+name=input("WHAT IS YOUR NAME? ")
+print(name)
+name="jack"
+print(name)
+name="pan"
+print(name)
+##length
+name=input("what is your name?")
+length=len(name)
+print(length)
+##swapping
+a=input("a=")
+b=input("b=")
+c=a 
+a=b 
+b=c
+print("a="+a)
+print("b="+b)
+##variables
+##unique and easy not to make the system confused
+name="jack"
+print(name)
+name="angela"
+print(name)
+user_name=input("what is your name?")
+l=len(user_name)
+print(l)
---
+++ project1.py
@@ -0,0 +1,5 @@
+print("Welcome to the Band name generator")
+city=input("What's name of the city you grew up in?")
+pet=input("What's your pet name ?")
+print("your name of the city could be " +city+" "+pet)
+
